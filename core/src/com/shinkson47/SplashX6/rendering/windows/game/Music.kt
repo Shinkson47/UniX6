6e737c95a169d5016235c34fdd08aa2452b2525f
Jordan T. Gray <personal@shinkson47.in>
2021-11-15 18:11:22 +0000

    Game window improvements for new skin
    Refactored game windows in anticipation of many more game windows.
    
    Altered the unit windows to better fit the new theme.
    
    KNOWN BUG : Spotify window is horribly broken. Something to do with colspans.

---
+++ core/src/com/shinkson47/SplashX6/rendering/windows/game/Music.kt
@@ -0,0 +1,48 @@
+package com.shinkson47.SplashX6.rendering.windows.game
+
+import com.badlogic.gdx.scenes.scene2d.ui.Label
+import com.shinkson47.SplashX6.audio.AudioController
+import com.shinkson47.SplashX6.rendering.StageWindow
+import com.shinkson47.SplashX6.utility.Assets
+
+// TODO - Display the currently playing track title, UI modifications
+/**
+ * # Window for in-game music UI
+ * @author Jordan Gray
+ */
+class Music : StageWindow("Music") {
+
+    override fun constructContent() {
+        if (FIRST_CONSTRUCTION) return
+
+        span(add(
+                Label("You can also control spotify!", Assets.SKIN)
+        ).fillX().center())
+
+        hsep()
+
+        addButton("pauseSong", false){  AudioController.pauseMusic() }.pack()
+        tooltip("ttPause")
+
+        addButton("resumeSong") { AudioController.resumeMusic() }
+        tooltip("ttResume")
+
+        hsep()
+
+        addButton("skipSong",false) { AudioController.nextSong() }
+        tooltip("ttSkip")
+
+        addButton("previousSong", false) {  AudioController.previousSong() }
+        tooltip("ttPrevious")
+
+        row()
+
+        addButton("resetPlaylist") { AudioController.resetPlaylist() }
+        tooltip("ttReset")
+        updateColSpans()
+    }
+
+    init {
+        constructContent()
+    }
+}
