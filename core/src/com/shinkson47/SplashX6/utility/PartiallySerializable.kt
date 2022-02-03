0230da4eb39a067b456423dc4e8c88b6e601dc49
Jordan T. Gray <business@shinkson47.in>
2022-02-03 20:13:47 +0000

    Changed structured help text to a xml.
    The engine and java do not allow for traversing directory trees within the exported jar. Instead, i'll use an plist with nested dictionaries to represent the same structure.

---
+++ core/src/com/shinkson47/SplashX6/utility/PartiallySerializable.kt
@@ -0,0 +1,9 @@
+package com.shinkson47.SplashX6.utility
+
+/**
+  * # TODO
+  * @author [Jordan T. Gray](https://www.shinkson47.in) on 03/02/2022
+  * @since v1
+  * @version 1 
+ */interface PartiallyTransient {
+}
